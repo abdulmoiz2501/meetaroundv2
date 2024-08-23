fa554af520ed301bf739d93c37e2736d39455f4a
Ismail <ilm68265@gmail.com>
2024-08-23 17:05:49 +0500

    All sockets done

--- lib/app/controllers/jam_controller.dart
+++ lib/app/controllers/jam_controller.dart
@@ -4,9 +4,12 @@ class JamController extends GetxController {
   RxString otherUserId = ''.obs;
   var isAccepted = Rx<bool?>(null);
   RxMap<String, dynamic> jamData = <String, dynamic>{}.obs;
+  var gotUrl = Rx<bool?>(null);
+
   void resetValues() {
     otherUserId.value = '';
     isAccepted.value = null;
+    gotUrl.value = null;
     jamData.clear();
     print('THe jam controller is resetted');
   }
--- lib/app/modules/JammingScreen/views/jamming_in_progress_view.dart
+++ lib/app/modules/JammingScreen/views/jamming_in_progress_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:scratch_project/app/controllers/jam_controller.dart';
+import 'package:scratch_project/app/modules/JammingScreen/controllers/jamming_screen_controller.dart';
 import 'package:scratch_project/app/utils/constraints/colors.dart';
 import 'package:scratch_project/app/utils/constraints/text_strings.dart';
 
@@ -13,46 +18,61 @@ class JammingInProgressView extends StatefulWidget {
 }
 
 class _JammingInProgressViewState extends State<JammingInProgressView> {
+  final JamController jamController = Get.find();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [VoidColors.primary, VoidColors.secondary],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 35.0.h, horizontal: 10.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 100.h,
-                    width: 100.w,
-                    child: Lottie.asset('assets/icons/UQejRQZqHg.json'),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    VoidTexts.jammingInProgress,
-                    style: GoogleFonts.poppins(
-                        fontSize: 18.spMax,
-                        color: VoidColors.whiteColor,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ],
+      body: Obx(() {
+        if (jamController.gotUrl.value != null) {
+          if (jamController.gotUrl.value == true) {
+            final JammingScreenController jammingScreenController =
+                Get.isRegistered()
+                    ? Get.find()
+                    : Get.put(JammingScreenController());
+            print(
+                '//////////This is the url: ${jamController.jamData['spotifyUrl']}////////');
+            jammingScreenController
+                .openSpotifyTrack(jamController.jamData['songUrl']);
+          }
+        }
+        return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [VoidColors.primary, VoidColors.secondary],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
           ),
-        ),
-      ),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 35.0.h, horizontal: 10.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 100.h,
+                      width: 100.w,
+                      child: Lottie.asset('assets/icons/UQejRQZqHg.json'),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      VoidTexts.jammingInProgress,
+                      style: GoogleFonts.poppins(
+                          fontSize: 18.spMax,
+                          color: VoidColors.whiteColor,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
